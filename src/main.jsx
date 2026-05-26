f34df8b2031540e3f264a29c12d351b4d079508f
drnicolasgomes-crypto <drnicolas.gomes@nicolasgomesadv.com.br>
2026-05-26 00:15:28 -0300

    Add main.jsx entry point

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import LexIA from "./LexIA"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LexIA />
+  </React.StrictMode>
+)
